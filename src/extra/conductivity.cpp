4d500b5d3603b6c212a6c74b58fb59241d74125f
Zhentao Wang <wztzjhn@gmail.com>
2019-03-19 14:53:22 -0400

    conductivity.cpp is now general for all models (no longer only triangular)

--- src/extra/conductivity.cpp
+++ src/extra/conductivity.cpp
@@ -19,8 +19,91 @@ std::vector<T> as_vector(ptree const& pt, ptree::key_type const& key)
     return r;
 }
 
+// same as in kondo.cpp. Should merge later.
+std::unique_ptr<Model> mk_model(const toml_ptr g) {
+    std::unique_ptr<Model> ret;
+    auto type = toml_get<std::string>(g, "model.type");
+    if (type == "simple") {
+        auto lattice = toml_get<std::string>(g, "model.lattice");
+        std::unique_ptr<SimpleModel> m;
+        if (lattice == "linear") {
+            m = SimpleModel::mk_linear(toml_get<int64_t>(g, "model.w"));
+        } else if (lattice == "square") {
+            m = SimpleModel::mk_square(toml_get<int64_t>(g, "model.w"), toml_get<int64_t>(g, "model.h"));
+        } else if (lattice == "triangular") {
+            m = SimpleModel::mk_triangular(toml_get<int64_t>(g, "model.w"), toml_get<int64_t>(g, "model.h"));
+        } else if (lattice == "kagome") {
+            m = SimpleModel::mk_kagome(toml_get<int64_t>(g, "model.w"), toml_get<int64_t>(g, "model.h"));
+        } else if (lattice == "cubic") {
+            m = SimpleModel::mk_cubic(toml_get<int64_t>(g, "model.lx"), toml_get<int64_t>(g, "model.ly"), toml_get<int64_t>(g, "model.lz"));
+        } else {
+            std::cerr << "Simple model lattice '" << lattice << "' not supported.\n";
+            std::exit(EXIT_FAILURE);
+        }
+        m->J  = toml_get<double>(g, "model.J");
+        m->t1 = toml_get<double>(g, "model.t1", 0);
+        m->t2 = toml_get<double>(g, "model.t2", 0);
+        m->t3 = toml_get<double>(g, "model.t3", 0);
+        ret = std::move(m);
+    } else if (type == "mostovoy") {
+        auto lattice = toml_get<std::string>(g, "model.lattice");
+        if (lattice != "cubic") {
+            std::cerr << "Mostovoy model requires `lattice = \"cubic\"`\n";
+            std::exit(EXIT_FAILURE);
+        }
+        auto m = std::make_unique<MostovoyModel>(toml_get<int64_t>(g, "model.lx"),
+                                                 toml_get<int64_t>(g, "model.ly"),
+                                                 toml_get<int64_t>(g, "model.lz"));
+        m->J     = toml_get<double>(g, "model.J");
+        m->t_pds = toml_get<double>(g, "model.t_pds");
+        m->t_pp  = toml_get<double>(g, "model.t_pp");
+        m->delta = toml_get<double>(g, "model.delta");
+        ret = std::move(m);
+    } else {
+        std::cerr << "Model type '" << type << "' not supported.\n";
+        std::exit(EXIT_FAILURE);
+    }
+    ret->kT_init  = toml_get<double>(g, "model.kT");
+    ret->kT_decay = toml_get<double>(g, "model.kT_decay", 0);
+    ret->zeeman   = {toml_get<double>(g, "model.zeeman_x",  0), toml_get<double>(g, "model.zeeman_y",  0), toml_get<double>(g, "model.zeeman_z",  0)};
+    ret->current  = {toml_get<double>(g, "model.current_x", 0), toml_get<double>(g, "model.current_y", 0), toml_get<double>(g, "model.current_z", 0)};
+    ret->easy_z   = toml_get<double>(g, "model.easy_z", 0);
+    ret->s0       = toml_get<double>(g, "model.s0", 0);
+    ret->s1       = toml_get<double>(g, "model.s1", 0);
+    ret->s2       = toml_get<double>(g, "model.s2", 0);
+    ret->s3       = toml_get<double>(g, "model.s3", 0);
+    
+    // setup dilute spin positions
+    int num_sites = ret->n_sites;
+    double filling_spins = toml_get<double>(g, "model.filling_spins", 1.0);
+    auto& spin_exist = ret->spin_exist;
+    assert(filling_spins > 0.0 && filling_spins <= 1.0);
+    if (std::abs(filling_spins-1.0) < 1e-10) {                                  // every site occupied
+        spin_exist.clear();
+    } else {                                                                    // some sites empty
+        spin_exist.assign(num_sites, false);
+        auto rng_spin = std::mt19937(toml_get<int64_t>(g, "random_seed"));
+        std::uniform_real_distribution<double> dist(0.0, 1.0);
+        std::cout << "Sites occupied by spins: " << std::endl;
+        for (int i = 0; i < num_sites; i++) {
+            if (dist(rng_spin) < filling_spins) {
+                spin_exist[i] = true;
+                std::cout << i << ",";
+            }
+        }
+        std::cout << std::endl;
+    }
+    ret->allow_update.clear();
+    for (int i = 0; i < num_sites; i++) {
+        if (spin_exist.empty() || spin_exist[i]) ret->allow_update.push_back(i);
+    }
+    std::cout << "spin concentration input:    " << filling_spins << std::endl;
+    std::cout << "spin concentration realized: "
+    << static_cast<double>(ret->allow_update.size()) / num_sites << std::endl;
+    return ret;
+}
 
-void triangular(int argc, char *argv[]) {
+void conductivity(int argc, char *argv[]) {
     auto engine = fkpm::mk_engine<cx_flt>();
     if (engine == nullptr) std::exit(EXIT_FAILURE);
     if (argc != 2) {
@@ -33,18 +116,7 @@ void triangular(int argc, char *argv[]) {
     std::cout << "using toml file `" << input_name << "`!\n";
     toml_ptr g = toml_from_file(input_name);
     
-    auto m = SimpleModel::mk_triangular(toml_get<int64_t>(g, "model.w"), toml_get<int64_t>(g, "model.h"));
-    m->J  = toml_get<double>(g, "model.J");
-    m->t1 = toml_get(g, "model.t1", 0.0);
-    m->t2 = toml_get(g, "model.t2", 0.0);
-    m->t3 = toml_get(g, "model.t3", 0.0);
-    m->kT_init  = toml_get<double>(g, "model.kT");
-    m->kT_decay = toml_get(g, "model.kT_decay", 0.0);
-    m->zeeman   = {toml_get(g, "model.zeeman_x", 0.0), toml_get(g, "model.zeeman_y", 0.0), toml_get(g, "model.zeeman_z", 0.0)};
-    m->easy_z   = toml_get(g, "model.easy_z", 0.0);
-    m->s1       = toml_get(g, "model.s1", 0.0);
-    m->s2       = toml_get(g, "model.s2", 0.0);
-    m->s3       = toml_get(g, "model.s3", 0.0);
+    auto m = mk_model(g);
     
     std::string json_name;
     std::cout << "Input dumpfile name:" << std::endl;
@@ -62,6 +134,7 @@ void triangular(int argc, char *argv[]) {
     while (std::getline(json_file, json_eachline)) {
         json_contents += json_eachline;
     }
+    json_file.close();
     std::istringstream is (json_contents);
     read_json (is, pt_json);
     auto time    = pt_json.get<int>    ("time");
@@ -69,97 +142,88 @@ void triangular(int argc, char *argv[]) {
     auto filling = pt_json.get<double> ("filling");
     auto mu      = pt_json.get<double> ("mu");
     auto spin    = as_vector<double>(pt_json, "spin");
-    
     std::cout << "lattice: " << toml_get<std::string>(g, "model.lattice") << std::endl;
     std::cout << "time:    " << time << std::endl;
     std::cout << "action:  " << action << std::endl;
     std::cout << "filling: " << filling << std::endl;
     std::cout << "mu:      " << mu << std::endl;
-    std::cout << "J:       " << m->J << std::endl;
-    std::cout << "t:       " << m->t1 << std::endl;
     std::cout << "kT:      " << m->kT() << std::endl;
     
-    assert(spin.size() == m->n_sites * 3);              // build spin configuration from dump file
+    // build spin configuration from dump file
+    assert(spin.size() == m->n_sites * 3);
     for (int i = 0; i < m->n_sites; i++) {
         m->spin[i] = vec3(spin[3*i],spin[3*i+1],spin[3*i+2]);
+        if (m->spin_exist.empty() || m->spin_exist[i]) {
+            assert(m->spin[i].norm2() > 1e-10);
+        } else {
+            assert(m->spin[i].norm2() < 1e-10);
+            // put (0,0,1) when running, to avoid potential normalization error
+            m->spin[i] = {0.0, 0.0, 1.0};
+        }
     }
     m->set_hamiltonian(m->spin);
-    
-    int purpose, M, Mq, use_correlated, n_colors, seed;
-    std::cout << "Inut 0/1/2 for calculating sigma_xx/sigma_xy/both: " << std::endl;
-    std::cin >> purpose;
     fkpm::EnergyScale es;
-    es = engine->energy_scale(m->H, 0.1);
-    std::cout << "energyscale: [" << es.lo << ", " << es.hi << "]" << std::endl;
+    es = engine->energy_scale(m->H, 0.05);
+    std::cout << "energyscale: [" << es.lo << ", " << es.hi << "]" << std::endl << std::endl;
+    
+    std::cout << "---------- parameters for conductivity calculation ----------" << std::endl;
+    vec3 dir1(1.0,0.0,0.0), dir2(1.0,0.0,0.0);
+    std::cout << "Input x,y,z direction of the 1st current operator (3 numbers separated by space):" << std::endl;
+    std::cin >> dir1.x >> dir1.y >> dir1.z;
+    std::cout << dir1 << std::endl;
+    std::cout << "Input x,y,z direction of the 2nd current operator (3 numbers separated by space):" << std::endl;
+    std::cin >> dir2.x >> dir2.y >> dir2.z;
+    std::cout << dir2 << std::endl;
+    auto j1 = m->electric_current_operator(m->spin, dir1);
+    auto j2 = m->electric_current_operator(m->spin, dir2);
+    
+    int M, Mq, n_colors, seed;
     std::cout << "Input M:" << std::endl;
     std::cin >> M;
     std::cout << M << std::endl;
     std::cout << "Input Mq:" << std::endl;
     std::cin >> Mq;
     std::cout << Mq << std::endl;
-    std::cout << "Input 0/1 for using uncorrelated/correlated random numbers:" << std::endl;
-    std::cin >> use_correlated;
-    std::cout << use_correlated << std::endl;
     std::cout << "Input n_colors:" << std::endl;
     std::cin >> n_colors;
     std::cout << n_colors << std::endl;
-    std::cout << "Input seed:" << std::endl;
+    std::cout << "Input random seed:" << std::endl;
     std::cin >> seed;
-    std::cout << seed << std::endl;
-    
+    std::cout << seed << std::endl << std::endl;
     fkpm::RNG rng(seed);
-    if (use_correlated) {
-        engine->set_R_correlated(m->groups(n_colors), rng);
-    } else {
-        engine->set_R_uncorrelated(m->H.n_rows, 2*n_colors, rng);
-    }
+    engine->set_R_uncorrelated(m->H.n_rows, 2*n_colors, rng);
     
     auto kernel = fkpm::jackson_kernel(M);
     engine->set_H(m->H, es);
     
-    auto jx = m->electric_current_operator(m->spin, {1,0,0});
-    auto jy = m->electric_current_operator(m->spin, {0,1,0});
-    
-    json_file.close();
-    
-    auto mu_dos = engine->moments(M);
-    auto gamma = fkpm::moment_transform(mu_dos, Mq);
+    // calculate dos
+    auto moments_dos = engine->moments(M);
+    auto gamma = fkpm::moment_transform(moments_dos, Mq);
     fkpm::Vec<double> mu_list, rho;
     fkpm::density_function(gamma, es, mu_list, rho);
     for (int i = 0; i < rho.size(); i++) {
         rho[i]/=m->n_sites;
     }
     
+    // calculate conductivity
     std::cout << "calculating moments2... " << std::flush;
     fkpm::timer[0].reset();
-    Vec<Vec<fkpm::cx_double>> mu_xx, mu_xy;
-    if (purpose == 0) {
-        mu_xx = engine->moments2_v1(M, jx, jx, 10, 16);
-        mu_xy = mu_xx; // invalid
-    } else if (purpose == 1){
-        mu_xy = engine->moments2_v1(M, jx, jy, 10, 16);
-        mu_xx = mu_xy; // invalid
-    } else {
-        mu_xx = engine->moments2_v1(M, jx, jx, 10, 16);
-        mu_xy = engine->moments2_v1(M, jx, jy, 10, 16);
-    }
+    Vec<Vec<fkpm::cx_double>> moments_sigma = engine->moments2_v1(M, j1, j2, 10, 16);
     cout << " done. " << fkpm::timer[0].measure() << "s.\n";
     
     cout << "calculating dc conductivities... " << std::flush;
-    std::ofstream fout2("full_time"+std::to_string(time)+"_M"+std::to_string(M)+"_corr"+std::to_string(use_correlated)+
+    std::ofstream fout2("full_time"+std::to_string(time)+"_M"+std::to_string(M)+
                         "_color"+std::to_string(n_colors)+"_seed"+std::to_string(seed)+".dat", std::ios::out);
     fout2 << std::scientific << std::right;
-    fout2 << std::setw(20) << "#(1)" << std::setw(20) << "(2)" << std::setw(20) << "(3)"
-          << std::setw(20) << "(4)" << std::setw(20) << "(5)" << std::setw(20) << "(6)" << std::endl;
-    fout2 << std::setw(20) << "M" << std::setw(20) << "kT" << std::setw(20) << "mu"
-          << std::setw(20) << "rho" << std::setw(20) << "sigma_xx" << std::setw(20) << "sigma_xy" << std::endl;
+    fout2 << std::setw(20) << "#(1)" << std::setw(20) << "(2)" << std::setw(20) << "(3)" << std::endl;
+    fout2 << std::setw(20) << "mu" << std::setw(20) << "rho" << std::setw(20) << "sigma" << std::endl;
     int interval = std::max(Mq/400,5);
     for (int i = 0; i < Mq; i+=interval) {
         auto cmn = electrical_conductivity_coefficients(M, Mq, m->kT(), mu_list[i], 0.0, es, kernel);
-        auto sigma_xx = std::real(fkpm::moment_product(cmn, mu_xx));
-        auto sigma_xy = std::real(fkpm::moment_product(cmn, mu_xy));
-        fout2 << std::setw(20) << M << std::setw(20) << m->kT() << std::setw(20) << mu_list[i]
-              << std::setw(20) << rho[i] << std::setw(20) << sigma_xx << std::setw(20) << sigma_xy << std::endl;
+        auto sigma = std::real(fkpm::moment_product(cmn, moments_sigma));
+        fout2 << std::setw(20) << mu_list[i]
+              << std::setw(20) << rho[i]
+              << std::setw(20) << sigma << std::endl;
     }
     fout2.close();
     cout << " done. " << fkpm::timer[0].measure() << "s.\n";
@@ -168,24 +232,23 @@ void triangular(int argc, char *argv[]) {
     if (! fin0.good()) {
         fin0.close();
         std::ofstream fout3("result_time"+std::to_string(time)+".dat", std::ios::out | std::ios::app );
-        fout3 << std::setw(20) << "#(1)" << std::setw(20) << "(2)" << std::setw(20) << "(3)"
-              << std::setw(20) << "(4)" << std::setw(20) << "(5)" << std::setw(20) << "(6)"
-              << std::setw(20) << "(7)" << std::setw(20) << "(8)" << std::endl;
-        fout3 << std::setw(20) << "M" << std::setw(20) << "correlated" << std::setw(20) << "colors"
-              << std::setw(20) << "seed" << std::setw(20) << "F" << std::setw(20) << "mu"
-              << std::setw(20) << "sigma_xx" << std::setw(20) << "sigma_xy" << std::endl;
+        fout3 << std::setw(20) << "#(1)" << std::setw(20) << "(2)"
+              << std::setw(20) <<  "(3)" << std::setw(20) << "(4)"
+              << std::setw(20) <<  "(5)" << std::setw(20) << "(6)" << std::endl;
+        fout3 << std::setw(20) << "M"    << std::setw(20) << "colors"
+              << std::setw(20) << "seed" << std::setw(20) << "F"
+              << std::setw(20) << "mu"   << std::setw(20) << "sigma" << std::endl;
         fout3.close();
     }
     std::ofstream fout3("result_time"+std::to_string(time)+".dat", std::ios::out | std::ios::app );
     fout3 << std::scientific << std::right;
-    auto cmn       = electrical_conductivity_coefficients(M, Mq, m->kT(), mu, 0.0, es, kernel);
+    auto cmn = electrical_conductivity_coefficients(M, Mq, m->kT(), mu, 0.0, es, kernel);
 
-    fout3 << std::setw(20) << M << std::setw(20) << use_correlated << std::setw(20) << n_colors
+    fout3 << std::setw(20) << M << std::setw(20) << n_colors
           << std::setw(20) << seed
           << std::setw(20) << fkpm::electronic_energy(gamma, es, m->kT(), filling, mu)/m->n_sites
           << std::setw(20) << mu
-          << std::setw(20) << std::real(fkpm::moment_product(cmn, mu_xx))
-          << std::setw(20) << std::real(fkpm::moment_product(cmn, mu_xy))
+          << std::setw(20) << std::real(fkpm::moment_product(cmn, moments_sigma))
           << std::endl;
     fout3.close();
     std::cout << std::endl;
@@ -194,7 +257,6 @@ void triangular(int argc, char *argv[]) {
 }
 
 
-
 int main(int argc, char *argv[]) {
-    triangular(argc, argv);
+    conductivity(argc, argv);
 }
